9ffd6ef3bf014d810f723f2e00a0b9db19b8e8e0
Alessandro <cavuoti.alessandro@tuttomail.com>
2018-12-09 21:44:06 +0100

    backend started on alessandro pc

--- front-end/src/app/card.service.ts
+++ front-end/src/app/card.service.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@angular/core';
 import {cardType} from './type-card-container';
 import {CARDS} from './mock-card';
 import { Observable, of } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  private heroesUrl = 'localhost:312/api/categories';  // URL to web api
+
+  constructor(private http: HttpClient) { }
 
   getCards(): Observable<cardType[]> {
-    return of (CARDS);
+    return this.http.get<cardType[]>(this.heroesUrl);
   }
 
   getCard(id: number): Observable<cardType> {
     return of(CARDS.find(card => card.id === id));
   }
-
-
 }
--- front-end/src/app/mock-card.ts
+++ front-end/src/app/mock-card.ts
@@ -1,11 +1,11 @@
 import {cardType} from './type-card-container';
 
 export const CARDS: cardType[] = [
-  {type: 'Email', other: 'other1', attributes: ['Libero', 'Gmail'], id: 1,},
-  {type: 'Business', other: 'other2', attributes: ['Trenitalia', 'Trello'], id: 2},
-  {type: 'Education', other: 'other3', attributes: ['Coursera', 'Udemy'], id: 3},
-  {type: 'Games', other: 'other4', attributes: ['Hearthstone', 'Magic'], id: 4},
-  {type: 'Entertainment', other: 'other5', attributes: ['Netflix', 'Youtube'], id: 5},
-  {type: 'Shopping', other: 'other6', attributes: ['Zalando', 'Amazon'], id: 6},
-  {type: 'Social', other: 'other7', attributes: ['Facebook', 'Instagram'], id: 7},
+  {type: 'Email', attributes: ['Libero', 'Gmail'], id: 1, url: ['http://www.libero.it', 'http://www.gmail.com']},
+  {type: 'Business', attributes: ['Trenitalia', 'Trello'], id: 2, url: ['http://www.trenitalia.com', 'https://trello.com']},
+  {type: 'Education', attributes: ['Coursera', 'Udemy'], id: 3, url: ['https://www.coursera.org', 'https://www.udemy.com']},
+  {type: 'Games', attributes: ['Hearthstone', 'Fifa'], id: 4, url: ['https://playhearthstone.com', 'https://www.fifa.com']},
+  {type: 'Entertainment', attributes: ['Netflix', 'Youtube'], id: 5, url: ['https://www.netflix.com', 'https://www.youtube.com']},
+  {type: 'Shopping', attributes: ['Zalando', 'Amazon'], id: 6, url: ['https://www.zalando.it', 'https://www.amazon.it']},
+  {type: 'Social', attributes: ['Facebook', 'Instagram'], id: 7, url: ['https://it-it.facebook.com', 'https://www.instagram.com']},
 ];
