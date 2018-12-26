8e79b40fe44e3c77decf86aef7c85eee9e0fc0a0
Alessandro <cavuoti.alessandro@tuttomail.com>
2018-12-26 23:09:20 +0100

    add comments

--- front-end/src/app/card.service.ts
+++ front-end/src/app/card.service.ts
@@ -9,15 +9,15 @@ import 'rxjs-compat/add/operator/do';
 })
 export class CardService {
 
-  private cardUrl = 'http://localhost:3000/api/setupGavpass/fullapi';  // URL to web api
+  private cardUrl = 'http://localhost:3000/api/setupGavpass/fullapi';
   private categoriesName = 'http://localhost:3000/api/setupGavpass/fullapi/categoriesName';
 
   constructor(private http: HttpClient) { }
-
+  // Get all the information of the card
   getCards(): Observable<cardType[]> {
     return this.http.get<cardType[]>(this.cardUrl).do(console);
   }
-
+  // Get name all list of the categories name
   getCategoriesName(): Observable<cardType[]> {
     return this.http.get<cardType[]>(this.categoriesName).do(console);
   }
