1408d055110a8bc6c25611c3053e1bcf3a608b0f
Alessandro <cavuoti.alessandro@tuttomail.com>
2019-02-02 19:44:25 +0100

    modali da fixare

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { DemoMaterialModule } from '../material-module';
 import { MatNativeDateModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CardComponent, AddSiteComponent, AddSiteSucessfullyComponent } from './card/card.component';
+import { CardComponent, AddSiteComponent } from './card/card.component';
 import { AppRoutingModule } from './app-routing.module';
 // tslint:disable-next-line:max-line-length
 import { CardDetailComponent, ModifySiteInsideComponent, AddSiteInsideComponent, DeleteSiteComponent } from './card-detail/card-detail.component';
@@ -24,7 +24,6 @@ import { MatDialogModule } from '@angular/material';
     ModifySiteInsideComponent,
     AddSiteInsideComponent,
     DeleteSiteComponent,
-    AddSiteSucessfullyComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +37,7 @@ import { MatDialogModule } from '@angular/material';
     MatDialogModule
   ],
   // tslint:disable-next-line:max-line-length
-  entryComponents: [CardComponent, CardDetailComponent, AddSiteComponent, ModifySiteInsideComponent, AddSiteInsideComponent, DeleteSiteComponent, AddSiteSucessfullyComponent],
+  entryComponents: [CardComponent, CardDetailComponent, AddSiteComponent, ModifySiteInsideComponent, AddSiteInsideComponent, DeleteSiteComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
